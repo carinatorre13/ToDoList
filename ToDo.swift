aee5df1a8d429f73cd6f5ba72e08c5cc8a7d55e1
Carina Torre <carinatorre13@gmail.com>
2020-07-14 10:56:13 -0400

    Initial Commit

---
+++ ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Carina Torre on 7/13/20.
+//  Copyright © 2020 Carina Torre. All rights reserved.
+//
+class ToDo{
+    var name = ""
+    var important = false
+}
+
+import Foundation
